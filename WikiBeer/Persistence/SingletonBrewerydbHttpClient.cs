c81371ac4361f6cff1b157bb6d056aa7f3975d04
SongWenhao <swh2106@hotmail.com>
2018-04-14 14:54:18 +0200

    add SingletonBreweryDbHttpClient class, it will create a singleton HttpClient to access brewerydb.com web api.

---
+++ WikiBeer/Persistence/SingletonBrewerydbHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WikiBeer.Persistence
+{
+    public class SingletonBreweryDbHttpClient
+    {
+        private static HttpClient _httpClient;
+        protected SingletonBreweryDbHttpClient() { }
+        public static HttpClient Instance()
+        {
+            if (_httpClient == null)
+            {
+                _httpClient = new HttpClient { BaseAddress = new Uri("http://api.brewerydb.com/v2/") };
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            return _httpClient;
+        }
+    }
+}
