e8b5c74729b734f4cbacf788606709c70ece1fc2
SongWenhao <swh2106@hotmail.com>
2018-04-15 13:14:39 +0200

    add mvc single select list helper.

---
+++ WikiBeer/Core/Helpers/SingleSelectList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WikiBeer.Core.Helpers
+{
+    public class SingleSelectList<TCollectionItem, TKey>
+    {
+        public SelectListItem[] Items { get; protected set; }
+
+        public SingleSelectList(
+            IEnumerable<TCollectionItem> items,
+            Func<TCollectionItem, TKey> getValue,
+            Func<TCollectionItem, string> getText,
+            TKey selectedValue = default(TKey))
+        {
+            Items = items
+                .Select(item =>
+                {
+                    var value = getValue(item);
+                    return new SelectListItem
+                    {
+                        Value = value.ToString(),
+                        Text = getText(item),
+                        Selected = value.Equals(selectedValue)
+                    };
+                })
+                .OrderBy(i => i.Text)
+                .ToArray();
+        }
+    }
+}
