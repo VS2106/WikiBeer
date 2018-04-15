1144fc6b467818fcde0827aaf2ee20d0910edb4f
SongWenhao <swh2106@hotmail.com>
2018-04-15 13:13:59 +0200

    add beers controller.

---
+++ WikiBeer/Controllers/BeersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WikiBeer.Core.Repositories;
+
+namespace WikiBeer.Controllers
+{
+    public class BeersController : Controller
+    {
+        private readonly Lazy<IBeerRepository> _beerRepo;
+        private readonly Lazy<IStyleRepository> _styleRepo;
+
+        public BeersController(Lazy<IBeerRepository> beerRepo,
+            Lazy<IStyleRepository> styleRepo)
+        {
+            _beerRepo = beerRepo;
+            _styleRepo = styleRepo;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var beers = await _beerRepo.Value.GetAllAsync();
+            var styles = await _styleRepo.Value.GetAllAsync();
+            return new EmptyResult();
+        }
+    }
+}
